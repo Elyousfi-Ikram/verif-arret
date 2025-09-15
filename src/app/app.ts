0a1c9b6ec1f9e8e65822252fc03d496240546094
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2025-09-15 17:52:35 +0200

    git init

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';;
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  constructor(private router: Router) { }
+}
