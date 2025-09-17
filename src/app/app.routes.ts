f30b321c607ec63873bf6e19923ba17251446e63
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2025-09-17 13:29:05 +0200

    ajout du composant decourvir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
-
+import { DecouvrirComponent } from './components/decouvrir/decouvrir';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'decouvrir', component: DecouvrirComponent },
+
 ];
---
+++ src/app/components/decouvrir/decouvrir.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decouvrir',
+  standalone: true,
+  imports: [],
+  templateUrl: './decouvrir.html',
+  styleUrls: ['./decouvrir.scss']
+})
+export class DecouvrirComponent {
+
+}
