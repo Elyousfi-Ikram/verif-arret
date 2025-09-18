f83378a3f80b4e37b0ad4c4c4c9ef412aa499492
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2025-09-18 16:12:21 +0200

    ajout du composant prestations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { DecouvrirComponent } from './components/decouvrir/decouvrir';
+import { PrestationsComponent } from './components/prestations/prestations';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'decouvrir', component: DecouvrirComponent },
+    { path: 'prestations', component: PrestationsComponent },
 
 ];
---
+++ src/app/components/prestations/prestations.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestations',
+  standalone: true,
+  imports: [],
+  templateUrl: './prestations.html',
+  styleUrls: ['./prestations.scss']
+})
+export class PrestationsComponent {
+  // Composant statique - plus de logique nécessaire
+}
