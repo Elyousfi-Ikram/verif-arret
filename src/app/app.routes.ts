1d7c8dafb6029d434921e710543accdd0eb834ac
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2025-09-19 12:16:41 +0200

    ajout du composant engagements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { DecouvrirComponent } from './components/decouvrir/decouvrir';
 import { PrestationsComponent } from './components/prestations/prestations';
-
+import { EngagementComponent } from './components/engagement/engagement';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'decouvrir', component: DecouvrirComponent },
     { path: 'prestations', component: PrestationsComponent },
+    { path: 'engagements', component: EngagementComponent },
+
 
 ];
---
+++ src/app/components/engagement/engagement.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-engagement',
+  standalone: true,
+  imports: [],
+  templateUrl: './engagement.html',
+  styleUrls: ['./engagement.scss']
+})
+export class EngagementComponent {
+}
